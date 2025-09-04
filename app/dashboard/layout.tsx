34eef0295dd44ba2fff6b2f1b64d65b302cb7fe1
Kingestif <kingestiff@gmail.com>
2025-09-04 12:08:11 +0300

    feat: add dark mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,16 @@
 import { SideBar } from "../ui/dashboard/sideBar";
 import { NavBar } from "../ui/navBar";
 
+/**
+ * ? min-h-screen on the parent div & flex-grow on the children might be Potential issue but works for now
+ */
 export default function Layout({children}:{children: React.ReactNode}) {
   return (
-    <div className="flex bg">
+    <div className="flex bg min-h-screen">
         <SideBar/>
-        <div className="flex-grow">
+        <div className="flex flex-col flex-grow min-h-screen">
           <NavBar name={"Overview"}/>
-          <div className="bg-gray-100 px-10 h-screen">{children}</div>   
+          <div className="bg-gray-100 px-10  dark:bg-gray-500 flex-grow">{children}</div>   
         </div> 
     </div>
   )
