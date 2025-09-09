d650576c4331766242cd0fa6301ec0fb4236e798
Kingestif <kingestiff@gmail.com>
2025-09-09 12:10:04 +0300

    refactor: remove unused imports from dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import { BlueCard } from "../ui/card";
-import { Chart } from "../ui/dashboard/chart";
 import FullCard from "../ui/dashboard/fullCard";
 import FullChart from "../ui/dashboard/fullChart";
 import FullPieChart from "../ui/dashboard/fullPieChart";
 import QuickTransfer from "../ui/dashboard/quickTransfer";
 import { Transactions } from "../ui/dashboard/transactions";
-import { LightCard } from "../ui/lightcard";
 
 export default function Dashboard(){
     return (
