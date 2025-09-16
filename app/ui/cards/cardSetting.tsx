e60f7b23791be3b523f9a9c50f546210bb33f5dd
Kingestif <kingestiff@gmail.com>
2025-09-16 22:31:37 +0300

    fix:  fix unique key error

--- app/ui/cards/cardSetting.tsx
+++ app/ui/cards/cardSetting.tsx
@@ -6,7 +6,7 @@ export default function CardSetting() {
         <div className="text-xl font-semibold">Card Setting</div>
         <div className="flex flex-col bg-white dark:bg-gray-800 rounded-2xl p-2">
             {CardSettingData.map((item,idx)=>(
-                <div key={item.icon} className="flex flex-col p-4 rounded-2xl items-center justify-between">
+                <div key={item.name} className="flex flex-col p-4 rounded-2xl items-center justify-between">
                     <div className="flex gap-5">
                         <div className="w-13 h-13 rounded-xl">
                             <img src={item.icon} alt="" />
