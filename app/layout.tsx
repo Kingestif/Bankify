3ade8407fd7e56a8329e1633a6ef464ab73d8211
Kingestif <kingestiff@gmail.com>
2025-09-17 11:31:40 +0300

    feat: add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+/**
+ * * The script Avoids Flash of Incorrect Theme at the start of page load
+ */
 export const metadata: Metadata = {
   title: "Bankify",
   description: "Make Banking simple & modern",
@@ -13,6 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(){try{const s=localStorage.getItem('theme');
+            if(s==='dark'||(!s&&window.matchMedia('(prefers-color-scheme: dark)').matches))
+            document.documentElement.classList.add('dark');}catch(e){}})();`
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
